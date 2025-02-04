b9a0a6980be8ecaef8f5c55239005b3da6bcb575
NAUFAL FEBRIANSYAH <113345776+NaufalFA13@users.noreply.github.com>
2025-02-04 23:58:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U<br>PUTRI').split('')
+    const titles = ('I LOVE U\nPUTRI').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
